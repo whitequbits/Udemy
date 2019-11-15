984c1b624d5b5c11550c9e79198a397946433063
rainoverme002 <bagus.gitdata@gmail.com>
2019-11-15 12:07:41 +0700

    DOM Events Exercise

--- Udemy Complete Web Developer/12 - DOM Manipulation/script.js
+++ Udemy Complete Web Developer/12 - DOM Manipulation/script.js
@@ -1,41 +1,78 @@
-// button and input is get by id in the html file
-var button = document.getElementById("enter");
+var enter_button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var delete_button = document.getElementsByClassName("delete");
 
-// function for the input length
+// Delete the item :
+
+// add every delete button an event listener to delete it
+for (let i = 0; i < delete_button.length; i++) {
+  delete_button[i].addEventListener("click", removeparents, false);
+}
+
+// remove the parents of the delete button
+function removeparents(event) {
+  event.target.removeEventListener("click", removeparents, false);
+  event.target.parentNode.remove();
+}
+
+// Get it Done :
+
+function getEventTarget(event) {
+  // it choose between the event or window.event
+  event = event || window.event;
+
+  // event.target refers to the clicked <li> element
+  return event.target || event.srcElement;
+}
+
+// add event listener for the ul
+ul.addEventListener("click", function(event) {
+  var target = getEventTarget(event);
+
+  // toggle the .done class from css so it will give the strikethrough
+  target.classList.toggle("done");
+});
+
+// Add the item :
+
+// check the input length
 function inputLength() {
   return input.value.length;
 }
 
-// function to create the li elements
-function createLiElements() {
-  // li is the li tag
+// Create the list element
+function createListElement() {
+  // create the li element
   var li = document.createElement("li");
-  // append the text child to the li as parent (attach the text to the li)
+  //  create the new delete button
+  var new_delete_button = document.createElement("button");
+  new_delete_button.innerHTML = "Delete";
+  // when it on click deleteit
+  new_delete_button.onclick = removeparents;
+
   li.appendChild(document.createTextNode(input.value));
-  // append the li to the ul (attach the li to the ul)
+  li.appendChild(new_delete_button);
   ul.appendChild(li);
   input.value = "";
 }
 
-// function to add the li when you click
-function addWhenClick() {
-  // it only work if the input length is greater than zero
+// Add list after the click if the input length greater than zero
+function addListAfterClick() {
   if (inputLength() > 0) {
-    createLiElements();
+    createListElement();
   }
 }
 
-function addWhenKeyPressed(events) {
-  // it only work if the input length is greater than zero
-  if (inputLength() > 0 && events.keyCode === 13) {
-    createLiElements();
+// Add list after press enter key if the input length greater than zero
+function addListAfterKeypress(event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
   }
 }
 
-// Using the click button
-button.addEventListener("click", addWhenClick);
+enter_button.addEventListener("click", addListAfterClick);
 
-// Using the enter in the keyboard
-input.addEventListener("keypress", addWhenKeyPressed);
+delete_button.addEventListener("click");
+
+input.addEventListener("keypress", addListAfterKeypress);
