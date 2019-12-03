b01a43211338ac20503bef7d39a7c14ffa5b4b58
rainoverme002 <bagus.gitdata@gmail.com>
2019-12-03 10:25:48 +0700

    Create Robot Cards

--- Udemy Complete Web Developer/Project/rubofriends/src/index.js
+++ Udemy Complete Web Developer/Project/rubofriends/src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Card from "./Card";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+import { robots } from "./robots";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// br3 is border 3
+// pa3 is padding 3
+// ma3 is margin 3
+
+ReactDOM.render(
+  <>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />,
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />,
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />,
+  </>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
