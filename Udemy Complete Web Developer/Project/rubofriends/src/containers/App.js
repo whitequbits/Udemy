13e5cbe88d3fef13b95fc91d29011e9766aeb2b7
rainoverme002 <bagus.gitdata@gmail.com>
2019-12-04 23:20:55 +0700

    Refactoring the code

--- Udemy Complete Web Developer/Project/rubofriends/src/containers/App.js
+++ Udemy Complete Web Developer/Project/rubofriends/src/containers/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { robots } from "./robots";
-import Cardlist from "./Cardlist";
-import Searchbox from "./Searchbox";
+import Cardlist from "../components/Cardlist";
+import Searchbox from "../components/Searchbox";
+import Scroll from "../components/Scroll";
 import "./App.css";
 
 class App extends React.Component {
@@ -28,22 +28,21 @@ class App extends React.Component {
   };
 
   render() {
-    const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    if (this.state.robots.length == 0) {
-      return <h1 className="tc">Please Wait</h1>;
-    } else {
-      return (
-        <div className="tc">
-          <h1 className="f1">Rubofriends</h1>
-          <Searchbox searchChange={this.onSearchChange} />
+    return !robots.length ? (
+      <h1 className="tc">Please Wait</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">Rubofriends</h1>
+        <Searchbox searchChange={this.onSearchChange} />
+        <Scroll>
           <Cardlist robots={filteredRobots} />
-        </div>
-      );
-    }
+        </Scroll>
+      </div>
+    );
   }
 }
 
--- Udemy Complete Web Developer/Project/rubofriends/src/index.js
+++ Udemy Complete Web Developer/Project/rubofriends/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import "tachyons";
-import App from "./App";
+import App from "./containers/App";
 
 // br3 is border 3
 // pa3 is padding 3
