bfdde1e247db38d5eff504824facbad1ed777d11
rainoverme002 <bagus.gitdata@gmail.com>
2019-12-07 08:09:25 +0700

    Update 234 - 235 Promises

---
+++ Udemy Complete Web Developer/20 - AJAX-HTTP-JSON/exercisePromises.js
@@ -0,0 +1,46 @@
+// Solve the questions below:
+
+// #1) Create a promise that resolves in 4 seconds and returns "success" string
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("success");
+  }, 4000);
+});
+// #2) Run the above promise and make it console.log "success"
+promise.then(console.log);
+// #3) Read about Promise.resolve() and Promise.reject(). How can you make
+// the above promise shorter with Promise.resolve() and console loggin "success"
+const promise2 = Promise.resolve(
+  setTimeout(() => {
+    console.log("success");
+  }, timeout)
+);
+
+// #4) Catch this error and console log 'Ooops something went wrong'
+Promise.reject("failed").catch("Ooops something went wrong");
+
+// #5) Use Promise.all to fetch all of these people from Star Wars (SWAPI) at the same time.
+// Console.log the output and make sure it has a catch block as well.
+const urls = [
+  "https://swapi.co/api/people/1",
+  "https://swapi.co/api/people/2",
+  "https://swapi.co/api/people/3",
+  "https://swapi.co/api/people/4"
+];
+
+Promise.all(
+  urls.map(url => {
+    fetch(url)
+      .then(people => people.json())
+      .then(array => {
+        console.log(array[0]);
+        console.log(array[1]);
+        console.log(array[2]);
+        console.log(array[3]);
+      })
+      .catch(error => console.log(error, "failed"));
+  })
+);
+
+// #6) Change one of your urls above to make it incorrect and fail the promise
+// does your catch block handle it?
