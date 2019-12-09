160615b1969a80e9f0484651014e7f97a873e2a5
rainoverme002 <bagus.gitdata@gmail.com>
2019-12-09 18:20:26 +0700

    Add splice method to class array

--- Udemy Master Data Structure and Algorithm/6 - Array/Create-class-array.js
+++ Udemy Master Data Structure and Algorithm/6 - Array/Create-class-array.js
@@ -25,6 +25,28 @@ class Array {
     }
     delete this.data[this.length - 1];
   }
+
+  unshift(input) {
+    for (let i = 0; i < this.length - 1; i++) {
+      this.data[i + 1] = this.data[i];
+    }
+    this.data[0] = input;
+    this.length++;
+  }
+
+  splice(index, dltitems, newitems) {
+    let i = 0;
+    while (i < dltitems) {
+      this.shift(index);
+      this.length--;
+      i++;
+    }
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i + 1] = this.data[i];
+    }
+    this.data[index] = newitems;
+    this.length++;
+  }
 }
 
 let array1 = new Array();
@@ -36,4 +58,8 @@ console.log(array1);
 array1.pop();
 console.log(array1);
 array1.delete(1);
+console.log(array1);
+array1.unshift("alien");
+console.log(array1);
+array1.splice(1, 1, "Hello");
 console.log(array1);
