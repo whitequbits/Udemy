58eb24bef277b9413e4eb0d9ca06bf20ee5035bb
rainoverme002 <bagus.gitdata@gmail.com>
2019-12-13 12:14:03 +0700

    Refactoring int Node Class and Add insert & print feature

--- Udemy Master Data Structure and Algorithm/8 - Linked List/Create-Linked-List.js
+++ Udemy Master Data Structure and Algorithm/8 - Linked List/Create-Linked-List.js
@@ -3,14 +3,17 @@
 
 // 10 --> 5 --> 16
 // head --> next --> tail
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
 class LinkedList {
   constructor(value) {
     // first of all this DS need Head
-    this.head = {
-      value: value,
-      next: null
-    };
+    this.head = new Node(value);
 
     // But because there is only one data in this so head == tail
     this.tail = this.head;
@@ -19,10 +22,7 @@ class LinkedList {
 
   append(value) {
     // create object new Node
-    const newNode = {
-      value: value,
-      next: null
-    };
+    const newNode = new Node(value);
     // Then the new Node is connect as this.tail.next value
     this.tail.next = newNode;
 
@@ -35,22 +35,58 @@ class LinkedList {
 
   prepend(value) {
     // we want create the new node
-    const newNode = {
-      value: value,
-      // next value is the former this.head
-      next: this.head
-    };
+    const newNode = new Node(value);
+
+    // next value is the former this.head
+    newNode.next = this.head;
 
     // and the new node become the new head
     this.head = newNode;
 
     this.length++;
   }
+
+  insert(index, value) {
+    if (index == 0) {
+      this.prepend(value);
+    } else if (index >= this.length) {
+      this.append(value);
+    } else {
+      let currentNode = this.head;
+      for (let j = 0; j < index; j++) {
+        var preNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      const nextNode = currentNode;
+      const newNode = new Node(value);
+      newNode.next = nextNode;
+      preNode.next = newNode;
+      this.length++;
+    }
+  }
+
+  printMyList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode != null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
-console.log(myLinkedList);
 myLinkedList.append(5);
+console.log(myLinkedList.printMyList());
 myLinkedList.append(16);
+console.log(myLinkedList.printMyList());
 myLinkedList.prepend(1);
+console.log(myLinkedList.printMyList());
+myLinkedList.insert(7, 100);
+console.log(myLinkedList.printMyList());
+myLinkedList.insert(0, 70);
+console.log(myLinkedList.printMyList());
+myLinkedList.insert(3, 88);
+console.log(myLinkedList.printMyList());
 console.log(myLinkedList);
