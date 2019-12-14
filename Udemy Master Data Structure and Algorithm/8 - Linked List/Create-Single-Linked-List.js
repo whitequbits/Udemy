9a0192b148aeff2bb97dec5336d9cdefffbb9298
rainoverme002 <bagus.gitdata@gmail.com>
2019-12-14 10:10:49 +0700

    Add reverse method

--- Udemy Master Data Structure and Algorithm/8 - Linked List/Create-Single-Linked-List.js
+++ Udemy Master Data Structure and Algorithm/8 - Linked List/Create-Single-Linked-List.js
@@ -54,23 +54,34 @@ class LinkedList {
   }
 
   insert(index, value) {
+    // check if index === 0 just use prepend
+    // check if index >= this.length just use append
+    // else use normal insert for linked list
     if (index == 0) {
       this.prepend(value);
     } else if (index >= this.length) {
       this.append(value);
     } else {
+      // check current node[index]
       const currentNode = this.traverse(index);
+      // check node[index -1]
       const preNode = this.traverse(index - 1);
+      // the next Node is our currentNode
       const nextNode = currentNode;
+      //define our new Node
       const newNode = new Node(value);
+      //our newNode.next is our former currentNode
       newNode.next = nextNode;
+      //our preNode.next is our newNode
       preNode.next = newNode;
       this.length++;
     }
   }
 
   traverse(index) {
+    //define from the head
     let currentNode = this.head;
+    //looping through the linkedlist
     for (let j = 0; j < index; j++) {
       currentNode = currentNode.next;
     }
@@ -78,6 +89,7 @@ class LinkedList {
   }
 
   search(value) {
+    //logic is the same with traverse
     let currentNode = this.head;
     while (currentNode !== null) {
       if (currentNode.value === value) {
@@ -89,6 +101,8 @@ class LinkedList {
   }
 
   remove(index) {
+    //for removing must check is it the head or the tail
+    //then if not both of it, prevNode.next is our nextNode
     const prevNode = this.traverse(index - 1);
     const nextNode = this.traverse(index + 1);
     if (index === 0) {
@@ -105,11 +119,46 @@ class LinkedList {
   printMyList() {
     const array = [];
     let currentNode = this.head;
+    // logic is the same with traverse
+    // but push it into the array
     while (currentNode !== null) {
       array.push(currentNode.value);
       currentNode = currentNode.next;
     }
     return array;
+  }
+
+  reverse() {
+    //check if there is only one item in linked list
+    if (!this.head.next) {
+      return this.head;
+    }
+
+    // 1->10->12->19
+    // first is = 1
+    // second is 10
+    // tail = 1
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+
+    // looping while second != null
+    while (second) {
+      // temp = 12
+      const temp = second.next;
+      // change the pointer from 1->10 become 1<-10
+      second.next = first;
+      // then first = 10
+      first = second;
+      // then second = 12
+      second = temp;
+      // after one looping 1<-10->12->19
+    }
+    // after all looping define that this.tail.next = null
+    this.tail.next = null;
+    // first become new head
+    this.head = first;
+    return this;
   }
 }
 
@@ -128,4 +177,6 @@ myLinkedList.insert(3, 88);
 console.log(myLinkedList.printMyList());
 console.log(myLinkedList.search(105));
 console.log(myLinkedList.remove(6));
+console.log(myLinkedList.printMyList());
+myLinkedList.reverse();
 console.log(myLinkedList.printMyList());
