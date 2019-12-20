70f9279d4ed2faba6bdd2f0f952976e421dc8e82
rainoverme002 <bagus.gitdata@gmail.com>
2019-12-20 11:49:32 +0700

    Add Create BST lookup method

--- Udemy Master Data Structure and Algorithm/10 - BST/Create-BST.js
+++ Udemy Master Data Structure and Algorithm/10 - BST/Create-BST.js
@@ -37,12 +37,31 @@ class BST {
       currentNode.right = newNode;
     }
   }
+
+  lookup(value) {
+    //check form the root
+    let currentNode = this.root;
+
+    // repeatable check
+    do {
+      if (currentNode.value === value) {
+        return "Yes I got it";
+      } else if (currentNode.value > value) {
+        currentNode = currentNode.left;
+      } else if (currentNode.value < value) {
+        currentNode = currentNode.right;
+      }
+    } while (currentNode.left !== null && currentNode.right !== null);
+    return "Sorry you can't find it";
+  }
 }
 
 const myBST = new BST();
 myBST.insert(12);
+console.log(myBST.lookup(12));
 myBST.insert(15);
 myBST.insert(8);
 myBST.insert(4);
 myBST.insert(10);
 console.log(myBST);
+console.log(myBST.lookup(12));
