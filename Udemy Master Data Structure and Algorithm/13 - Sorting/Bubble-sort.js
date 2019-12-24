3b292183a06101a830effcad1117445bb274db05
rainoverme002 <bagus.gitdata@gmail.com>
2019-12-24 11:41:43 +0700

    Add Bubble Sort excercise

---
+++ Udemy Master Data Structure and Algorithm/13 - Sorting/Bubble-sort.js
@@ -0,0 +1,26 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function bubbleSort(array) {
+  //Using two loops O(nˆ2)
+  //First one is to make sure it iterate until the array sorted
+  //Second one is to compare every element in the array
+  //Unique Logic :
+  //We know that every each iteration, the element on near the last item is sorted
+  //So we don't need to compare until the end of the array
+  //That's why we use j < array.length - (1 + i)
+
+  for (let i = 0; i < array.length - 1; i++) {
+    let passingData;
+    for (let j = 0; j < array.length - (1 + i); j++) {
+      if (array[j] > array[j + 1]) {
+        passingData = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = passingData;
+      }
+    }
+  }
+  return array;
+}
+
+bubbleSort(numbers);
+console.log(numbers);
