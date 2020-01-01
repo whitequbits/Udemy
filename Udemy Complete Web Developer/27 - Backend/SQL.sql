0019f10b9c5e8ec8c8344908353d195268dbbcd8
rainoverme002 <bagus.gitdata@gmail.com>
2020-01-01 21:32:37 +0700

    Update Backend Section

---
+++ Udemy Complete Web Developer/27 - Backend/SQL.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users(
+  id serial PRIMARY KEY,
+  name VARCHAR(100),
+  email text UNIQUE NOT NULL,
+  entries BIGINT DEFAULT 0,
+  joined TIMESTAMP NOT NULL 
+);
+
+CREATE TABLE login(
+  id serial PRIMARY KEY,
+  hash VARCHAR(100) NOT NULL,
+  email text UNIQUE NOT NULL
+);
